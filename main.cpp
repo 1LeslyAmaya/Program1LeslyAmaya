3a1f21a70f922e4b42166e1a996573026d2ae5b4
LeslyAmaya <lamayal@miumg.edu.gt>
2025-02-14 22:20:30 -0600

    Tarea_no2

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//primer program en c++
+#include <iostream>
+using namespace std;
+int main() {
+    int numero = 15;
+    float flotante =10.45;
+    double mayor = 15.32253;
+    char letra = 'a';
+    cout << letra;
+    return 0;
+}
