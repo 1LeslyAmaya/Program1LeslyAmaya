e35f053c236170de36b7cc7b4572d185461ea128
LeslyAmaya <lamayal@miumg.edu.gt>
2025-02-15 04:56:02 -0600

    Paso1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 //Estructura de control condicionales (IF - IF ELSE)
+
 #include <iostream>
 using namespace std;
+int main() {
+    int edad;
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+
+    if (edad >= 18) {
+        cout << "Eres mayor de edad." << endl;
+    } else {
+        cout << "Eres menor de edad." << endl;
+    }
+
+    return 0;
+}
 
